1d5c08ab7740655a6d1ab69b3893ef0bf36ebf8b
Matchobas <maalexandria@hotmail.com>
2023-12-25 18:46:57 -0300

    fix: broken torrent file

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import fs from 'fs';
-import decode from './decode.js';
+// import decode from './decode.js';
 
-const torrent = decode(fs.readFileSync('puppy.torrent'));
-console.log(torrent.announce.toString('utf8'));
+const torrent = fs.readFileSync('puppy.torrent');
+console.log(torrent.toString('utf8'));
+
+// const torrent = decode(fs.readFileSync('puppy.torrent'), 'utf8');
+// console.log(torrent.announce.toString('utf8'));
