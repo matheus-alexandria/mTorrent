1298e23856014624e42dc2a193f651e0279d6d5d
Matchobas <maalexandria@hotmail.com>
2023-12-30 23:01:29 -0300

    feat: create build connection request function

--- src/tracker.ts
+++ src/tracker.ts
@@ -1,4 +1,5 @@
 import dgram from 'node:dgram';
+import crypto from 'node:crypto';
 import { decode } from './utils/decode.js';
 
 export function getPeers(torrent: any, callback: (peers: any) => any) {
@@ -30,7 +31,16 @@ function respType(resp) {
 }
 
 function buildConnReq() {
-  // ...
+  const buf = Buffer.alloc(16);
+
+  buf.writeUInt32BE(0x417, 0);
+  buf.writeUInt32BE(0x27101980, 4);
+
+  buf.writeUInt32BE(0, 8);
+
+  crypto.randomBytes(4).copy(buf, 12);
+
+  return buf;
 }
 
 function parseConnResp(resp) {
