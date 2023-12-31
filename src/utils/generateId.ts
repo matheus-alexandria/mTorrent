4823f007810b90fd6859cfe3662b7498a2b27949
Matchobas <maalexandria@hotmail.com>
2023-12-31 13:26:36 -0300

    chore: change peer id

--- src/utils/generateId.ts
+++ src/utils/generateId.ts
@@ -5,7 +5,7 @@ let id: Buffer | null = null;
 export function generateId(): Buffer {
   if (!id) {
     id = crypto.randomBytes(20);
-    Buffer.from('-AT0001-').copy(id, 0);
+    Buffer.from('-MT0001-').copy(id, 0);
   }
   return id;
 };
