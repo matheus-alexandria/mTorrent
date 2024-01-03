05b5270dd1dba34d8287a62499729e4402f08ad9
Matchobas <maalexandria@hotmail.com>
2024-01-03 14:23:16 -0300

    feat: create infoHash function to hash info with sha1

--- src/torrentParser.ts
+++ src/torrentParser.ts
@@ -1,6 +1,13 @@
 import fs from 'node:fs';
-import { decode } from './index/decode.js';
+import { createHash } from 'node:crypto';
+import { decode } from './libs/decode';
+import { encode } from './libs/encode';
 
 export function open(filePath: string) {
   return decode(fs.readFileSync(filePath), 'utf8');
+}
+
+export function infoHash(torrent: any) {
+  const info = encode(torrent.info);
+  return createHash('sha1').update(info).digest();
 }
