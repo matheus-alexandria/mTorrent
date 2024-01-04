605bc0d46786c63f8df4cbb75a6392c364795830
Matchobas <maalexandria@hotmail.com>
2024-01-04 11:28:22 -0300

    feat: create size function to check torrent file size

--- src/torrentParser.ts
+++ src/torrentParser.ts
@@ -11,3 +11,14 @@ export function infoHash(torrent: any) {
   const info = encode(torrent.info);
   return createHash('sha1').update(info).digest();
 }
+
+export function size(torrent: Record<string, any>): Buffer {
+  const size = torrent.info.files ?
+    torrent.info.files.map((file: Buffer) => file.length).reduce((a: number, b: number) => a + b) :
+    torrent.info.length;
+
+  const buf = Buffer.alloc(8);
+  buf.writeBigInt64BE(BigInt(size));
+
+  return buf;
+}
