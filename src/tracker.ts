1c29ae29c4dbe440b440dc00eecac2c765992af9
Matchobas <maalexandria@hotmail.com>
2024-01-05 22:15:08 -0300

    chore: pretify tracker file

--- src/tracker.ts
+++ src/tracker.ts
@@ -1,37 +1,43 @@
-import dgram from 'node:dgram';
-import crypto from 'node:crypto';
-import { infoHash, size } from './torrentParser';
-import { generateId } from './utils/generateId.js';
+import dgram from "node:dgram";
+import crypto from "node:crypto";
+import { infoHash, size } from "./torrentParser";
+import { generateId } from "./utils/generateId.js";
 
-export function getPeers(torrent: any, callback: (peers: any) => any) {
-  const socket = dgram.createSocket('udp4');
-  const url: string = torrent.announce.toString('utf8');
+export function getPeers(torrent: any, callback: (peers: Peer[]) => any) {
+  const socket = dgram.createSocket("udp4");
+  const url: string = torrent.announce.toString("utf8");
 
   udpSend(socket, buildConnRequest(), url);
 
-  socket.on('message', response => {
-    if (respType(response) === 'connect') {
+  socket.on("message", (response) => {
+    console.log(response);
+    if (respType(response) === "connect") {
       const connResp = parseConnResponse(response);
 
       const announceReq = buildAnnounceReq(connResp.connectionId, torrent);
       udpSend(socket, announceReq, url);
-    } else if (respType(response) === 'announce') {
+    } else if (respType(response) === "announce") {
       const announceResp = parseAnnounceResp(response);
       callback(announceResp.peers);
     }
   });
 }
 
-function udpSend(socket: dgram.Socket, message: Buffer, rawUrl: string, callback=()=>{}) {
+function udpSend(
+  socket: dgram.Socket,
+  message: Buffer,
+  rawUrl: string,
+  callback = () => {}
+) {
   const url = new URL(rawUrl);
-  socket.send(message, Number(url.port), url.host, callback);
+  socket.send(message, 0, message.length, Number(url.port), url.host, callback);
 }
 
 function respType(resp: Buffer): string {
   const action = resp.readUInt32BE(0);
-  if (action === 0) return 'connect';
-  if (action === 1) return 'announce';
-  return '';
+  if (action === 0) return "connect";
+  if (action === 1) return "announce";
+  return "";
 }
 
 function buildConnRequest(): Buffer {
@@ -53,11 +59,11 @@ function parseConnResponse(response: Buffer): parseConnResponseResult {
   return {
     action: response.readUint32BE(0),
     transactionId: response.readUint32BE(4),
-    connectionId: response.subarray(8)
-  }
+    connectionId: response.subarray(8),
+  };
 }
 
-function buildAnnounceReq(connId: Buffer, torrent: any, port=6881): Buffer {
+function buildAnnounceReq(connId: Buffer, torrent: any, port = 6881): Buffer {
   const buf = Buffer.allocUnsafe(98);
 
   // connection id
@@ -104,30 +110,30 @@ function parseAnnounceResp(resp: Buffer): parseAnnounceResponseResult {
     transactionId: resp.readUInt32BE(4),
     leechers: resp.readUInt32BE(8),
     seeders: resp.readUInt32BE(12),
-    peers: group(resp.subarray(20), 6).map(address => {
+    peers: group(resp.subarray(20), 6).map((address) => {
       return {
-        ip: address.subarray(0, 4).join('.'),
-        port: address.readUInt16BE(4)
-      }
-    })
-  }
+        ip: address.subarray(0, 4).join("."),
+        port: address.readUInt16BE(4),
+      };
+    }),
+  };
 }
 
 type parseConnResponseResult = {
-  action: number; 
+  action: number;
   transactionId: number;
   connectionId: Buffer;
-}
+};
 
 type parseAnnounceResponseResult = {
-  action: number; 
+  action: number;
   transactionId: number;
   leechers: number;
   seeders: number;
-  peers: Peer[]
-}
+  peers: Peer[];
+};
 
 type Peer = {
   ip: string;
   port: number;
-}
+};
